f0c9a007353c8b972807fc3fbdc8ac9e82383cb0
stefkudla <stefankudla@gmail.com>
2022-06-09 11:37:58 -0700

    Untrack files in .gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ff4d0180ea8b9427"; }
+/******/ 		__webpack_require__.h = function() { return "d8d825c584393f67"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
