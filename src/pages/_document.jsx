662754a385213f4887e9d46b2796dc0aea99d9d4
stefkudla <stefankudla@gmail.com>
2022-06-13 16:59:57 -0700

    move stylesheet link from index to doc to remove warning

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -4,7 +4,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 const Document = () => {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
+        />
+      </Head>
       <Meta />
       <body>
         <Main />
