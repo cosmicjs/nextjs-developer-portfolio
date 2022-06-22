318a7e0921ad3a2b82c149245278adef8d33fab8
stefkudla <stefankudla@gmail.com>
2022-06-21 18:23:59 -0700

    add PostMeta

--- src/pages/posts/[slug].jsx
+++ src/pages/posts/[slug].jsx
@@ -3,11 +3,11 @@ import PostBody from '@/components/PostBody'
 import PostHeader from '@/components/PostHeader'
 import { getAllPostsWithSlug, getPostAndMorePosts } from '@/lib/cosmic'
 import PostTitle from '@/components/PostTitle'
-import Head from 'next/head'
 import markdownToHtml from '@/lib/markdownToHtml'
 import AlertPreview from '@/components/AlertPreview'
 import PageNotFound from '../404'
 import Loader from '@/components/Loader'
+import { PostMeta } from '@/components/Meta'
 
 const Post = ({ post }) => {
   const router = useRouter()
@@ -24,24 +24,13 @@ const Post = ({ post }) => {
         </PostTitle>
       ) : (
         <>
-          <Head>
-            <title>{post.title}</title>
-            <meta property="og:title" content={post.title} />
-            <meta property="og:type" content="article" />
-            <meta
-              property="og:image"
-              content={post.metadata.cover_image.imgix_url}
-            />
-            <meta property="og:description" content={post.metadata.excerpt} />
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:site" content="@CosmicJS" />
-            <meta name="twitter:title" content={post.title} />
-            <meta name="twitter:description" content={post.metadata.excerpt} />
-            <meta
-              name="twitter:image"
-              content={post.metadata.cover_image.imgix_url}
-            />
-          </Head>
+          <PostMeta
+            title={post.title}
+            description={post.metadata.excerpt}
+            slug={post.slug}
+            page="posts"
+            imageUrl={post.metadata.cover_image.imgix_url}
+          />
           <article className="border-b border-back-subtle py-8 mb-8">
             {post.status === 'draft' ? (
               <AlertPreview preview={true} />
