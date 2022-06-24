2db1949a8e4563297aada72c10e0b2d32b67352b
stefkudla <stefankudla@gmail.com>
2022-06-23 18:23:42 -0700

    move layout component to pages / persist preview alert

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,54 +4,66 @@ import SocialIcons from '@/components/SocialIcons'
 import Image from 'next/image'
 import avatar from '../../public/images/avatar_4.png'
 import { PageMeta } from '@/components/Meta'
+import Layout from '@/components/Layout'
+import { getAllPosts } from '@/lib/cosmic'
 
-const About = () => {
+const About = ({ preview }) => {
   return (
     <>
       <PageMeta
         title="About | Developer Portfolio"
         description="About the developer"
       />
-      <section>
-        <h1 className="text-2xl md:text-3xl mb-12 font-bold">About Me</h1>
-        <div className="flex flex-col md:flex-row-reverse border-b pb-12">
-          <div className="flex-1 relative">
-            <Image
-              src={avatar}
-              alt="Stefan Kudla"
-              quality={85}
-              layout="responsive"
-              className="rounded-md"
-              placeholder="blur"
-              priority
-            />
-          </div>
-          <div className="flex-1 mt-12 md:mt-0 flex flex-col justify-start gap-y-8 pr-20">
-            <p>I love solving problems!</p>
-            <p>
-              My name is Stefan Kudla. Originally from rainy Seattle,
-              Washington, I now reside in the dry valley of Las Vegas, Nevada.
-              When I&apos;m not writing code, you can usually find me brushing
-              my teeth with coffee or looking for the best view atop a mountain.
-            </p>
-            <p>Get in touch to create something awesome together!</p>
-            <div className="flex items-center md:mt-6">
-              <a
-                href="/your_resume_here.pdf"
-                className="flex items-center mr-4 text-fore-primary border-2 border-accent w-fit px-4 py-1 rounded cursor-pointer hover:text-accent transition-colors"
-              >
-                <span className="mr-2">
-                  <PaperIcon />
-                </span>
-                Resume
-              </a>
-              <SocialIcons />
+      <Layout preview={preview}>
+        <section>
+          <h1 className="text-2xl md:text-3xl mb-12 font-bold">About Me</h1>
+          <div className="flex flex-col md:flex-row-reverse border-b pb-12">
+            <div className="flex-1 relative">
+              <Image
+                src={avatar}
+                alt="Stefan Kudla"
+                quality={85}
+                layout="responsive"
+                className="rounded-md"
+                placeholder="blur"
+                priority
+              />
+            </div>
+            <div className="flex-1 mt-12 md:mt-0 flex flex-col justify-start gap-y-8 pr-20">
+              <p>I love solving problems!</p>
+              <p>
+                My name is Stefan Kudla. Originally from rainy Seattle,
+                Washington, I now reside in the dry valley of Las Vegas, Nevada.
+                When I&apos;m not writing code, you can usually find me brushing
+                my teeth with coffee or looking for the best view atop a
+                mountain.
+              </p>
+              <p>Get in touch to create something awesome together!</p>
+              <div className="flex items-center md:mt-6">
+                <a
+                  href="/your_resume_here.pdf"
+                  className="flex items-center mr-4 text-fore-primary border-2 border-accent w-fit px-4 py-1 rounded cursor-pointer hover:text-accent transition-colors"
+                >
+                  <span className="mr-2">
+                    <PaperIcon />
+                  </span>
+                  Resume
+                </a>
+                <SocialIcons />
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </Layout>
     </>
   )
+}
+
+export async function getStaticProps({ preview = null }) {
+  const allPosts = (await getAllPosts(preview, 'posts')) || []
+  return {
+    props: { allPosts, preview },
+  }
 }
 
 export default About
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,31 +6,34 @@ import WorksSection from '@/sections/WorksSection'
 import PostsSection from '@/sections/PostsSection'
 import ContactSection from '@/sections/ContactSection'
 import { PageMeta } from '@/components/Meta'
+import Layout from '@/components/Layout'
 
-const Index = ({ allPosts, allWorks }) => {
+const Index = ({ allPosts, allWorks, preview }) => {
   return (
     <>
       <PageMeta
         title="Cosmic | Developer Portfolio"
         description="Developer Portfolio Template built with Next.js and Cosmic"
       />
-      <IntroSection />
-      <AboutMeSection />
-      <ToolboxSection />
-      <WorksSection posts={allWorks} />
-      <PostsSection posts={allPosts} />
-      <ContactSection />
+      <Layout preview={preview}>
+        <IntroSection />
+        <AboutMeSection />
+        <ToolboxSection />
+        <WorksSection posts={allWorks} />
+        <PostsSection posts={allPosts} />
+        <ContactSection />
+      </Layout>
     </>
   )
 }
 
 // Below, we are calling the function in our cosmic.js file, and getting the posts from our Cosmic bucket. Since we are fetching from a single function in our cosmic.js file, we state whether we are viewing a preview of our page, what kind of posts we want (since we have blog posts and works posts), and the amount of posts we want to grab. In this case on the home page, I specified a limit of 3 posts.
 
-export async function getStaticProps({ preview }) {
+export async function getStaticProps({ preview = null }) {
   const allPosts = (await getAllPosts(preview, 'posts', 3)) || []
   const allWorks = (await getAllPosts(preview, 'works', 3)) || []
   return {
-    props: { allPosts, allWorks },
+    props: { allPosts, allWorks, preview },
   }
 }
 export default Index
