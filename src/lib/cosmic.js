c65ed8cf33d6e800bf88bb701788dbf7f5499bf2
stefkudla <stefankudla@gmail.com>
2022-09-08 17:34:21 -0400

    Update to chaining methods and wrap in try/catch pattern

--- src/lib/cosmic.js
+++ src/lib/cosmic.js
@@ -12,63 +12,59 @@ const bucket = api.bucket({
 const is404 = error => /not found/i.test(error.message)
 
 export async function getPreviewPostBySlug(slug) {
-  const params = {
-    query: { slug },
-    status: 'any',
-    props: 'slug',
-  }
-
   try {
-    const data = await bucket.getObjects(params)
+    const data = await bucket.objects
+      .find({
+        slug: slug,
+      })
+      .props('slug')
+      .status('any')
     return data.objects[0]
   } catch (error) {
-    // Don't throw if a slug doesn't exist
     if (is404(error)) return
     throw error
   }
 }
 
-export async function getAllPostsWithSlug() {
-  const params = {
-    query: { type: 'posts' },
-    props: 'title,slug,metadata,created_at',
-  }
-  const data = await bucket.getObjects(params)
-  return data.objects
-}
-
 export async function getAllPosts(preview, postType, postCount) {
-  const params = {
-    query: { type: postType },
-    ...(preview && { status: 'any' }),
-    props:
-      'title,slug,metadata.category,metadata.excerpt,metadata.published_date,created_at,status',
-    limit: postCount,
-    sort: '-created_at',
+  try {
+    const data = await bucket.objects
+      .find({
+        type: postType,
+      })
+      .props(
+        'title,slug,metadata.category,metadata.excerpt,metadata.published_date,created_at,status'
+      )
+      .limit(postCount)
+      .sort('-created_at')
+      .status(preview ? 'any' : 'published')
+    return data.objects
+  } catch (error) {
+    if (is404(error)) return
+    throw error
   }
-  const data = await bucket.getObjects(params)
-  return data.objects
 }
 
 export async function getPostAndMorePosts(slug, preview) {
-  const singleObjectParams = {
-    query: { slug: slug },
-    ...(preview && { status: 'any' }),
-    props: 'slug,title,metadata,created_at',
-  }
-  const moreObjectParams = {
-    query: { type: 'posts' },
-    ...(preview && { status: 'any' }),
-    limit: 3,
-    props: 'title,slug,metadata,created_at',
-  }
-
   try {
-    const data = await bucket.getObjects(singleObjectParams)
-    const moreObjects = await bucket.getObjects(moreObjectParams)
+    const data = await bucket.objects
+      .find({
+        slug: slug,
+      })
+      .props('slug,title,metadata,created_at')
+      .status(preview ? 'any' : 'published')
+
+    const moreObjects = await bucket.objects
+      .find({
+        type: 'posts',
+      })
+      .props('slug,title,metadata,created_at')
+      .status(preview ? 'any' : 'published')
+
     const morePosts = moreObjects.objects
       ?.filter(({ slug: object_slug }) => object_slug !== slug)
       .slice(0, 2)
+
     return {
       post: data?.objects[0],
       morePosts,
@@ -80,10 +76,15 @@ export async function getPostAndMorePosts(slug, preview) {
 }
 
 export async function getAllCategories(category) {
-  const params = {
-    query: { type: category },
-    props: 'title',
+  try {
+    const data = await bucket.objects
+      .find({
+        type: category,
+      })
+      .props('title')
+    return data.objects
+  } catch (error) {
+    if (is404(error)) return
+    throw error
   }
-  const data = await bucket.getObjects(params)
-  return data.objects
 }
